82207cca75d7fb16d003567fa6356d62c86c3bcf
Youcef-Abdelliche <hy_abdelliche@esi.dz>
2021-06-30 13:58:41 +0100

    first commit13

--- project2cs_dpgr/settings.py
+++ project2cs_dpgr/settings.py
@@ -135,7 +135,7 @@ MEDIA_URL = '/icons/'
 
 MEDIA_URL = '/css/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
